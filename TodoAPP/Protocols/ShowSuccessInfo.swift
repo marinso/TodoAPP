531493ead8e1c1f62cbc667ec1f65d88a572bafd
Martin Nasierowski <mnasierowski@netkodo.com>
2020-06-08 21:58:10 +0200

    Protocols added

---
+++ TodoAPP/Protocols/ShowSuccessInfo.swift
@@ -0,0 +1,12 @@
+//
+//  ShowSuccessInfo.swift
+//  TodoAPP
+//
+//  Created by Martin Nasierowski on 07/06/2020.
+//  Copyright © 2020 Martin Nasierowski. All rights reserved.
+//
+import Foundation
+
+protocol ShowSuccessInfo {
+    func showSuccessInfo()
+}
